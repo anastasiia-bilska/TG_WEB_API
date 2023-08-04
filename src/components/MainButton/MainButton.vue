6bcd82fc53755a9f286fa3040cf343cca8a7d2fd
Anastasiia Bilska <nastya.bilskaya@gmail.com>
2023-08-04 15:11:46 +0300

    update the main button

--- src/components/MainButton/MainButton.vue
+++ src/components/MainButton/MainButton.vue
@@ -1,26 +1,121 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
+import MainButtonOptions from '@/components/MainButton/MainButtonOptions/MainButtonOptions.vue';
 
 const telegram = (window as any).Telegram;
 
-const buttonText = ref<String>(telegram.WebApp.MainButton.text);
+const buttonTextCustom = ref('');
 
-const changeMainButton = (option: String) => {};
+const buttonText = ref<string>(telegram.WebApp.MainButton.text);
+const buttonColor = ref<string>(telegram.WebApp.MainButton.color);
+const buttonTextColor = ref<string>(telegram.WebApp.MainButton.textColor);
+const buttonIsVisible = ref<boolean>(telegram.WebApp.MainButton.isVisible);
+const buttonIsActive = ref<boolean>(telegram.WebApp.MainButton.isActive);
+const buttonIsProgressVisible = ref<boolean>(telegram.WebApp.MainButton.isProgressVisible);
+
+const setText = () => {
+  telegram.WebApp.MainButton.setText(buttonTextCustom.value);
+  buttonText.value = telegram.WebApp.MainButton.text;
+
+  buttonTextCustom.value = '';
+};
+
+const showButton = () => {
+  telegram.WebApp.MainButton.show();
+  buttonIsVisible.value = telegram.WebApp.MainButton.isVisible;
+}
+
+const hideButton = () => {
+  telegram.WebApp.MainButton.hide();
+  buttonIsVisible.value = telegram.WebApp.MainButton.isVisible;
+}
 </script>
 
 <template>
-  <section class="container">
+  <section class="main-btn container">
     <h2 class="mb-4">MainButton</h2>
 
-    <article class="mb-4">
-      <p>
-        <i><strong>MainButton</strong></i> - this <i>object</i> controls the main button, which is
-        displayed at the bottom of the Web App in the Telegram interface.
+    <MainButtonOptions
+      :buttonText="buttonText"
+      :buttonColor="buttonColor"
+      :buttonTextColor="buttonTextColor"
+      :buttonIsVisible="buttonIsVisible"
+      :buttonIsActive="buttonIsActive"
+      :buttonIsProgressVisible="buttonIsProgressVisible"
+    />
+
+    <article class="mb-5">
+      <p class="mb-2">
+        <i><strong>show()</strong></i> - a method to make the button visible.
       </p>
 
-      <p>{{ buttonText }}</p>
+      <p class="mb-3">
+        <i><strong>hide()</strong></i> - a method to hide the button.
+      </p>
+
+      <div class="card p-3 shadow-sm">
+        <button class="mb-2 main-btn__show" type="button" @click="showButton">Show the MainButton</button>
+        <button type="button" class="main-btn__hide" @click="hideButton">Hide the MainButton</button>
+      </div>
+    </article>
+
+    <article class="mb-5">
+      <p class="mb-3">
+        <i><strong>setText(text)</strong></i> - a method to set the button text.
+      </p>
+
+      <div class="card p-3 shadow-sm">
+        <label class="d-block mb-2 fw-medium" for="main-btn-text">Set new MainButton text:</label>
+        <div class="input-group">
+          <input
+            v-model="buttonTextCustom"
+            type="text"
+            id="main-btn-text"
+            name="main-btn-text"
+            class="form-control"
+            placeholder="Type new MainButton text..."
+            aria-label="new MainButton text"
+          />
+          <button class="main-btn__input-btn" :disabled="buttonTextCustom.length < 1" type="button" @click="setText">change</button>
+        </div>
+      </div>
     </article>
   </section>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import '@/assets/sassStyles/colors';
+@import '@/assets/sassStyles/placeholders';
+
+.main-btn {
+  &__main-btn-options {
+    margin-bottom: 40px;
+  }
+
+  &__input-btn {
+    padding: 10px;
+    color: $color__main;
+    background-color: $color__telegram;
+    border: 1px solid transparent;
+    border-radius: 0 0.375rem 0.375rem 0;
+
+    transition: background-color 0.2s ease-in-out, opacity 0.2s ease-in-out;
+
+    &[disabled] {
+      opacity: 0.6;
+    }
+
+    &:active {
+      background-color: $color__accent;
+    }
+  }
+
+  &__show {
+    @extend %button-soft;
+  }
+
+  &__hide {
+    @extend %button-heavy;
+  }
+}
+</style>
---
+++ src/components/MainButton/MainButtonOptions/MainButtonOptions.vue
@@ -0,0 +1,145 @@
+<script lang="ts" setup>
+import { toRefs, reactive } from 'vue';
+
+const props = defineProps({
+  buttonText: {type: String, required: true },
+  buttonColor: {type: String, required: true},
+  buttonTextColor: {type: String, required: true},
+  buttonIsVisible: {type: Boolean, required: true},
+  buttonIsActive: {type: Boolean, required: true},
+  buttonIsProgressVisible: {type: Boolean, required: true},
+});
+
+// const buttonText = ref<string>(telegram.WebApp.MainButton.text);
+// const buttonColor = ref<string>(telegram.WebApp.MainButton.color);
+// const buttonTextColor = ref<string>(telegram.WebApp.MainButton.textColor);
+// const buttonIsVisible = ref<boolean>(telegram.WebApp.MainButton.isVisible);
+// const buttonIsActive = ref<boolean>(telegram.WebApp.MainButton.isActive);
+// const buttonIsProgressVisible = ref<boolean>(telegram.WebApp.MainButton.isProgressVisible);
+
+// onUpdated(() => {
+//   buttonText.value = telegram.WebApp.MainButton.text;
+// buttonColor.value = telegram.WebApp.MainButton.color;
+// buttonTextColor.value = telegram.WebApp.MainButton.textColor;
+// buttonIsVisible.value = telegram.WebApp.MainButton.isVisible;
+// buttonIsActive.value = telegram.WebApp.MainButton.isActive;
+// buttonIsProgressVisible.value = telegram.WebApp.MainButton.isProgressVisible;
+// })
+</script>
+
+<template>
+      <article class="main-btn__main-btn-options main-btn-options">
+      <p class="mb-3">
+        <i><strong>MainButton</strong></i> - this <i>object</i> controls the main button, which is
+        displayed at the bottom of the Web App in the Telegram interface.
+      </p>
+
+      <ul class="mb-4">
+        <li class="mb-2">
+          <i><strong>MainButton.text</strong></i> - <span>{{'<String>'}}</span> - current button text. Set to <i class="main-btn-options__marked">CONTINUE</i> by default.
+        </li>
+
+        <li class="mb-2">
+          <i><strong>MainButton.color</strong></i> - <span>{{'<String>'}}</span> - current button color. Set to <i class="main-btn-options__marked">themeParams.button_color</i> by default.
+        </li>
+
+        <li class="mb-2">
+          <i><strong>MainButton.textColor</strong></i> - <span>{{'<String>'}}</span> - current button text color. Set to <i class="main-btn-options__marked">themeParams.button_text_color</i>by default.
+        </li>
+
+        <li class="mb-2">
+          <i><strong>MainButton.isVisible</strong></i> - <span>{{'<Boolean>'}}</span> - shows whether the button is visible. Set to 
+            <i class="main-btn-options__marked">
+              false
+            </i>
+            by default.
+        </li>
+
+        <li class="mb-2">
+          <i><strong>MainButton.isActive</strong></i> - <span>{{'<Boolean>'}}</span> - shows whether the button is active. Set to
+            <i class="main-btn-options__marked">
+              true
+            </i>
+            by default.
+        </li>
+
+        <li class="mb-2">
+          <i><strong>MainButton.isProgressVisible</strong></i> - <span>{{'<Boolean Readonly>'}}</span> - shows whether the button is displaying a loading indicator.
+        </li>
+      </ul>
+
+      <div class="card p-3 shadow-sm">
+        <h3 class="mb-4 fs-5 text-center fw-bold">Current MainButton</h3>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Key</th>
+              <th scope="col">Value</th>
+              <th scope="col">Demo</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">text</th>
+              <td>{{ props.buttonText }}</td>
+              <td>-</td>
+            </tr>
+            <tr>
+              <th scope="row">color</th>
+              <td>{{ props.buttonColor }}</td>
+              <td>
+                <span
+                  class="main-btn-options__table-color"
+                  :style="{ 'background-color': buttonColor }"
+                ></span>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">textColor</th>
+              <td>{{ props.buttonTextColor }}</td>
+              <td><span class="main-btn-options__table-color"></span></td>
+            </tr>
+            <tr>
+              <th scope="row">isVisible</th>
+              <td>{{ props.buttonIsVisible }}</td>
+              <td>-</td>
+            </tr>
+            <tr>
+              <th scope="row">isActive</th>
+              <td>{{ props.buttonIsActive }}</td>
+              <td>-</td>
+            </tr>
+            <tr>
+              <th scope="row">isProgressVisible</th>
+              <td>{{ props.buttonIsProgressVisible }}</td>
+              <td>-</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </article>
+</template>
+
+<style lang="scss">
+@import '@/assets/sassStyles/colors';
+.main-btn-options {
+  &__marked {
+    display: inline-block;
+    padding: 0 6px;
+
+    background-color: $color__warning-light;
+    border-radius: 6px;
+  }
+
+  &__table-color {
+    display: inline-block;
+    height: 20px;
+    width: 100%;
+
+    vertical-align: middle;
+
+    border-radius: 5px;
+    border: 1px solid $color__primary;
+  }
+}
+</style>
