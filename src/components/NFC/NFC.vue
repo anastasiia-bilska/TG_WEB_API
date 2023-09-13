4daca359b3002ca8f4dd1cf11d57870df27c8067
Anastasiia Bilska <nastya.bilskaya@gmail.com>
2023-09-13 08:12:39 +0300

    update nfc

--- src/components/NFC/NFC.vue
+++ src/components/NFC/NFC.vue
@@ -1,9 +1,48 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 
 const isSupported = computed(() => {
   return !!('NDEFReader' in window);
 });
+
+const drugStoreNumber = ref<String | null>('');
+const adminLastName = ref('');
+
+const handleSelect = (event: Event) => {
+  const target = event.target as HTMLSelectElement;
+
+  drugStoreNumber.value = target.value || null;
+  console.log(drugStoreNumber.value);
+};
+
+const writeNFC = async () => {
+  const encoder = new TextEncoder();
+
+  if ('NDEFReader' in window) {
+    const ndef = new NDEFReader();
+
+    try {
+      await ndef.write({
+        records: [
+          {
+            recordType: 'mime',
+            mediaType: 'application/json',
+            data: encoder.encode(
+              JSON.stringify({
+                number: drugStoreNumber.value,
+                admin: adminLastName.value
+              })
+            )
+          }
+        ]
+      });
+
+      alert(`write() success!✅`);
+    } catch (error) {
+      alert(`write() error: ${error}❌`);
+    }
+  }
+};
 </script>
 
 <template>
@@ -12,8 +51,6 @@ const isSupported = computed(() => {
 
     <article class="mb-5">
       <h3>NDEFReader</h3>
-      <p>Detecting if Web NFC is supported can be done by checking the NDEFReader object.</p>
-
       <p class="mb-3">
         <i><code>if ("NDEFReader" in window) { ... }</code></i> - a way to detect if Web NFC is
         supported.
@@ -22,10 +59,57 @@ const isSupported = computed(() => {
       <div class="card p-3 shadow-sm d-flex">
         <p
           class="nfc__support p-2 m-0 rounded"
-          :class="{ 'nfc__success': isSupported, 'nfc__failture': !isSupported }"
+          :class="{ nfc__success: isSupported, nfc__failture: !isSupported }"
         >
           {{ isSupported ? 'NFC is supported! ✅' : 'NFC is not supported ❌' }}
         </p>
+      </div>
+    </article>
+
+    <article class="mb-5">
+      <h3>write()</h3>
+
+      <p class="mb-3">
+        <i><code>ndef.write(message)</code></i> - attempts to write an NDEF message to a tag.
+      </p>
+
+      <div class="card p-3 shadow-sm d-flex">
+        <h4 class="mb-3 fs-6">Record store data:</h4>
+        <div class="form-floating mb-3">
+          <select
+            class="form-select"
+            id="floatingSelect"
+            aria-label="Floating label select"
+            @change="handleSelect"
+          >
+            <option selected disabled>Open this select menu</option>
+            <option value="111">111</option>
+            <option value="123">123</option>
+            <option value="144">144</option>
+          </select>
+          <label for="floatingSelect">Select store number</label>
+        </div>
+
+        <div class="form-floating mb-4">
+          <input
+            v-model="adminLastName"
+            type="text"
+            class="form-control nfc__last-name"
+            placeholder="Username"
+            aria-label="Username"
+            aria-describedby="addon-wrapping"
+          />
+          <label for="floatingSelect" class="text-secondary">Admin last name</label>
+        </div>
+        <button
+          :disabled="adminLastName.length < 2 || !drugStoreNumber"
+          type="button"
+          class="nfc__button nfc__button--record"
+          :class="{ 'nfc__button--disabled': adminLastName.length < 2 || !drugStoreNumber }"
+          @click="writeNFC"
+        >
+          Record!
+        </button>
       </div>
     </article>
   </section>
